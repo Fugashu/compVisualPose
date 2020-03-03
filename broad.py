b05286ae816246b75bbd13c34a0e52f71ab78dfc
Jonas Frießlich <friesslich.jonas@gmail.com>
2020-03-03 11:59:51 +0100

    Update broad.py

--- broad.py
+++ broad.py
@@ -1,17 +1,11 @@
-#!/usr/bin/env python
 #for our dataset publish necessary tf
+#!/usr/bin/env python
 import rospy
-# to get commandline arguments
 import sys
-# because of transformations
 import tf
 import tf2_ros
 import geometry_msgs.msg
-
-if __name__ == '__main__':
-        rospy.init_node('my_static_tf2_broadcaster')
-        broadcaster = tf2_ros.StaticTransformBroadcaster()
-        '''static_transformStamped = geometry_msgs.msg.TransformStamped()
+'''static_transformStamped = geometry_msgs.msg.TransformStamped()
         static_transformStamped.header.stamp = rospy.Time.now()
         static_transformStamped.header.frame_id = "base_link"
         static_transformStamped.child_frame_id = "camera_link"
@@ -23,29 +17,18 @@ if __name__ == '__main__':
         static_transformStamped.transform.rotation.y = quat[1]
         static_transformStamped.transform.rotation.z = quat[2]
         static_transformStamped.transform.rotation.w = quat[3]'''
-        static_transformStamped = geometry_msgs.msg.TransformStamped()
-        static_transformStamped.header.stamp = rospy.Time.now()
-        static_transformStamped.header.frame_id = "camera_link"
-        static_transformStamped.child_frame_id = "asus"
-        static_transformStamped.transform.translation.x = 0.0
-        static_transformStamped.transform.translation.y = 0.0 #0.015
-        static_transformStamped.transform.translation.z = 0.0
-        quat = tf.transformations.quaternion_from_euler(-3.1415926535897931/2,0,-3.1415926535897931/2)
-        static_transformStamped.transform.rotation.x = quat[0]
-        static_transformStamped.transform.rotation.y = quat[1]
-        static_transformStamped.transform.rotation.z = quat[2]
-        static_transformStamped.transform.rotation.w = quat[3]
+if __name__ == '__main__':
+        rospy.init_node('my_static_tf2_broadcaster')
+        broadcaster = tf2_ros.StaticTransformBroadcaster()              
+		#Due to the fact that the algorithms need instant access to these transformations we didn't use the tf topic to provide this data
         static_transformStamped2 = geometry_msgs.msg.TransformStamped()
         static_transformStamped2.header.stamp = rospy.Time.now()
-        #due to the fact that the coordinate system of our algorithms looks different from the realsensesystem
         static_transformStamped2.header.frame_id = "camera_link" 
         static_transformStamped2.child_frame_id = "camera_color_optical_frame"
         static_transformStamped2.transform.translation.x = 0.0
         static_transformStamped2.transform.translation.y = 0.015
         static_transformStamped2.transform.translation.z = 0.0
-        #quat = tf.transformations.quaternion_from_euler(-0.785, -1.571, -0.785)
         quat = tf.transformations.quaternion_from_euler(-3.1415926535897931/2, 0, -3.1415926535897931/2)
-        #print quat[0],quat[1],quat[2],quat[3]
         static_transformStamped2.transform.rotation.x = quat[0]
         static_transformStamped2.transform.rotation.y = quat[1]
         static_transformStamped2.transform.rotation.z = quat[2]
@@ -56,27 +39,12 @@ if __name__ == '__main__':
         static_transformStamped5.child_frame_id = "camera_imu_optical_frame"
         static_transformStamped5.transform.translation.x = 0.0117399999872
         static_transformStamped5.transform.translation.y = 0.00552000012249
-        static_transformStamped5.transform.translation.z = -0.00510000018403
-        quat = tf.transformations.quaternion_from_euler(0, 0, 0)
+        static_transformStamped5.transform.translation.z = -0.00510000018403        
         static_transformStamped5.transform.rotation.x = -0.5
         static_transformStamped5.transform.rotation.y = 0.5
         static_transformStamped5.transform.rotation.z = -0.5
         static_transformStamped5.transform.rotation.w = 0.5
-        static_transformStamped3 = geometry_msgs.msg.TransformStamped()
-        static_transformStamped3.header.stamp = rospy.Time.now()
-        
-        static_transformStamped3.header.frame_id = "base_footprint"
-        #odom2 here because this is gonna be the startpoint of viso for ccny
-        static_transformStamped3.child_frame_id = "odom2" 
-        static_transformStamped3.transform.translation.x = 0.078
-        static_transformStamped3.transform.translation.y = 0.0175
-        static_transformStamped3.transform.translation.z = 0.715
-        quat = tf.transformations.quaternion_from_euler(0,0,0)
-        static_transformStamped3.transform.rotation.x = quat[0]
-        static_transformStamped3.transform.rotation.y = quat[1]
-        static_transformStamped3.transform.rotation.z = quat[2]
-        static_transformStamped3.transform.rotation.w = quat[3]
-        static_transformStamped4 = geometry_msgs.msg.TransformStamped()
+		static_transformStamped4 = geometry_msgs.msg.TransformStamped()
         static_transformStamped4.header.stamp = rospy.Time.now()
         static_transformStamped4.header.frame_id = "base_footprint"
         #odom2 here because this is gonna be the startpoint of viso for ccny
@@ -90,7 +58,39 @@ if __name__ == '__main__':
         static_transformStamped4.transform.rotation.y = quat[1]
         static_transformStamped4.transform.rotation.z = quat[2]
         static_transformStamped4.transform.rotation.w = quat[3]
-        #for turtlebot
+		
+        
+        broadcaster.sendTransform([static_transformStamped2,static_transformStamped4,static_transformStamped5])
+        
+        rospy.spin()
+		'''some algorithms required other names for the child frame id you find them below
+		static_transformStamped3 = geometry_msgs.msg.TransformStamped()
+        static_transformStamped3.header.stamp = rospy.Time.now()        
+        static_transformStamped3.header.frame_id = "base_footprint"
+        #odom2 here because this is gonna be the startpoint of viso for ccny
+        static_transformStamped3.child_frame_id = "odom2" 
+        static_transformStamped3.transform.translation.x = 0.078
+        static_transformStamped3.transform.translation.y = 0.0175
+        static_transformStamped3.transform.translation.z = 0.715
+        quat = tf.transformations.quaternion_from_euler(0,0,0)
+        static_transformStamped3.transform.rotation.x = quat[0]
+        static_transformStamped3.transform.rotation.y = quat[1]
+        static_transformStamped3.transform.rotation.z = quat[2]
+        static_transformStamped3.transform.rotation.w = quat[3]             
+		static_transformStamped = geometry_msgs.msg.TransformStamped()
+        static_transformStamped.header.stamp = rospy.Time.now()
+        static_transformStamped.header.frame_id = "camera_link"
+        static_transformStamped.child_frame_id = "asus"
+        static_transformStamped.transform.translation.x = 0.0
+        static_transformStamped.transform.translation.y = 0.0 #0.015
+        static_transformStamped.transform.translation.z = 0.0
+        quat = tf.transformations.quaternion_from_euler(-3.1415926535897931/2,0,-3.1415926535897931/2)
+        static_transformStamped.transform.rotation.x = quat[0]
+        static_transformStamped.transform.rotation.y = quat[1]
+        static_transformStamped.transform.rotation.z = quat[2]
+        static_transformStamped.transform.rotation.w = quat[3]'''		
+		#rosrun tf static_transform_publisher 0.0929 0.0325 0.0705 1.5707963 0 -1.5707963 base_footprint Opencv 100
+		#for turtlebot
         '''static_transformStamped4.transform.translation.x = 0.078
         static_transformStamped4.transform.translation.y = 0.0175
         static_transformStamped4.transform.translation.z = 0.715
@@ -99,10 +99,3 @@ if __name__ == '__main__':
         static_transformStamped4.transform.rotation.y = quat[1]
         static_transformStamped4.transform.rotation.z = quat[2]
         static_transformStamped4.transform.rotation.w = quat[3]'''
-        #rosrun tf static_transform_publisher 0.0929 0.0325 0.0705 1.5707963 0 -1.5707963 base_footprint Opencv 100
-
-        #broadcaster.sendTransform([static_transformStamped,static_transformStamped2])
-        broadcaster.sendTransform([static_transformStamped,static_transformStamped2,static_transformStamped3,static_transformStamped4,static_transformStamped5])
-        #broadcaster.sendTransform([static_transformStamped3])
-
-        rospy.spin()
